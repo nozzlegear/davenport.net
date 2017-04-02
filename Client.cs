a5ee1caed9231cc8656912b57d0682cc065adccd
Joshua Harms <joshua@nozzlegear.com>
2017-04-01 22:56:23 -0500

    Added get, exists, post, put, copy, delete methods.
    Curren conundrum is figuring out the best way to port the selectors from JavaScript. I'm thinking some kind of linq configuration that builds out the query, so (c => c.Foo = 'value1'; c.Bar = 'value2'). I think FluentValidation has something like this, might be worth taking a peek at their repo to see how it's done.

--- Client.cs
+++ Client.cs
@@ -3,6 +3,10 @@ using Flurl;
 using Flurl.Http;
 using System;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Davenport.Infrastructure;
+using Newtonsoft.Json;
+using Davenport.Entities;
 
 namespace Davenport
 {
@@ -20,7 +24,7 @@ namespace Davenport
             Config = config ?? new Configuration();
         }
 
-        protected IFlurlClient PrepareRequest(string path)
+        protected IFlurlClient PrepareRequest(string path, string rev = null)
         {
             var client = Url.Combine(Config.CouchUrl, Config.DatabaseName, path).AllowAnyHttpStatus();
 
@@ -28,15 +32,103 @@ namespace Davenport
             {
                 client = client.WithBasicAuth(Config.Username, Config.Password);
             }
+
+            if (! string.IsNullOrEmpty(rev))
+            {
+                client.Url.QueryParams.Add("rev", rev);
+            }
             
             return client;
         }
 
-        protected async Task CheckErrorAndGetBody()
+        /// <summary>
+        /// Takes a Flurl request client and executes it, sending along any provided content.
+        /// </summary>
+        /// <param name="client">The Flurl request client. This will be automatically disposed.</param>
+        /// <param name="method">Method used for the request.</param>
+        /// <param name="content">Optional content sent along with POST or PUT requests.</param>
+        protected async Task<T> ExecuteRequestAsync<T>(IFlurlClient client, HttpMethod method, HttpContent content = null)
         {
+            using (client)
+            {
+                var request = client.SendAsync(method, content);
+                var result = await request;
+                var rawBody = await request.ReceiveString();
 
+                if (! result.IsSuccessStatusCode)
+                {
+                    var message = $"Error with {client} request for CouchDB database {Config.DatabaseName} at {client.Url.ToString()}. {result.StatusCode} {result.ReasonPhrase}";
+                    var ex = new DavenportException(message)
+                    {
+                        StatusCode = (int) result.StatusCode,
+                        StatusText = result.ReasonPhrase,
+                        ResponseBody = rawBody,
+                        Url = client.Url.ToString(),   
+                    };
+                    
+                    throw ex;
+                }
+
+                return JsonConvert.DeserializeObject<T>(rawBody);
+            }
         }
         
+        /// <summary>
+        /// Gets a document with the given <paramref name="id" /> and optional <paramref name="rev" />.
+        /// </summary>
+        public async Task<DocumentType> GetAsync(string id, string rev = null)
+        {
+            var request = PrepareRequest(id, rev);
+
+            return await ExecuteRequestAsync<DocumentType>(request, HttpMethod.Get);
+        }
+
+        /// <summary>
+        /// Creates a document and assigns a random id.
+        /// </summary>
+        public async Task<PostPutCopyResponse> PostAsync(DocumentType doc)
+        {
+            var content = new JsonContent(doc);
+            var request = PrepareRequest("");
+
+            return await ExecuteRequestAsync<PostPutCopyResponse>(request, HttpMethod.Post, content);
+        }
+
+        /// <summary>
+        /// Updates or creates a document with the given <paramref name="id" />.
+        /// </summary>
+        public async Task<PostPutCopyResponse> PutAsync(string id, DocumentType doc, string rev = null)
+        {
+            var content = new JsonContent(doc);
+            var request = PrepareRequest(id, rev);
+
+            return await ExecuteRequestAsync<PostPutCopyResponse>(request, HttpMethod.Put, content);
+        }
+
+        /// <summary>
+        /// Checks whether a document with the given <paramref name="id" /> and optional <paramref name="rev" /> exists.
+        /// </summary>
+        public async Task<bool> ExistsAsync(string id, string rev = null)
+        {
+            using (var request = PrepareRequest(id, rev))
+            {
+                var result = await request.HeadAsync();
+
+                return result.IsSuccessStatusCode;
+            }
+        }
+
+        /// <summary>
+        /// Copies the document with the given <paramref name="id" /> and assigns the <paramref name="newId" /> to the copy.
+        /// </summary>
+        public async Task<PostPutCopyResponse> CopyAsync(string id, string newId)
+        {
+            var request = PrepareRequest(id).WithHeader("Destination", newId);
+            var method = new HttpMethod("COPY");
+
+            return await ExecuteRequestAsync<PostPutCopyResponse>(request, method);
+        }
+
         /**
          * Deletes the document with the given id and revision id.
          */
@@ -47,12 +139,9 @@ namespace Davenport
                 Warning?.Invoke(this, $"No revision specified for Davenport.DeleteAsync method with id ${id}. This may cause a document conflict error.");
             }
 
-            
-            //var result = await Axios.delete(this.databaseUrl + id, {
-            //    params: { rev }
-            //});
+            var request = PrepareRequest(id, rev);
 
-            //await CheckErrorAndGetBody(result);
+            await ExecuteRequestAsync<object>(request, HttpMethod.Delete);
         }
     }
 }
---
+++ Infrastructure/JsonContent.cs
@@ -0,0 +1,22 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Davenport.Infrastructure
+{
+    public class JsonContent : ByteArrayContent
+    {
+        public JsonContent(object content) : base (ToBytes(content))
+        {
+            Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+
+        static byte[] ToBytes(object content)
+        {
+            var rawData = JsonConvert.SerializeObject(content);
+            
+            return Encoding.UTF8.GetBytes(rawData);
+        }
+    }
+}
