e5c896e87a3a5295862d2885eef59c351db8ce06
Joshua Harms <joshua@nozzlegear.com>
2017-04-11 14:29:47 -0500

    Move nuget packing to build script.

--- ps/before_deploy.ps1
+++
@@ -1,6 +0,0 @@
-dotnet pack --no-build -c Release Davenport/davenport.csproj;
-
-$nupkg = (gci Davenport/bin/Release/*.nupkg)[0];
-
-# Push the nuget package to AppVeyor's artifact list.
-Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "davenport.nupkg";
--- ps/build_script.ps1
+++ ps/build_script.ps1
@@ -1,2 +1,8 @@
 dotnet restore;
 dotnet build -c Release;
+dotnet pack --no-build -c Release Davenport/davenport.csproj;
+
+$nupkg = (gci Davenport/bin/Release/*.nupkg)[0];
+
+# Push the nuget package to AppVeyor's artifact list.
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "davenport.nupkg";
