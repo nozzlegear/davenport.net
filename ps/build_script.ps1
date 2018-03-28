e0d8c3eee6bf292fccbaa37203d3be1ba212817e
Joshua Harms <joshua@nozzlegear.com>
2018-03-27 21:12:16 -0500

    Fix test scripts.

--- ps/build_script.ps1
+++ ps/build_script.ps1
@@ -1,11 +1,8 @@
 dotnet restore;
 dotnet build -c Release;
-dotnet pack --no-build -c Release Davenport/davenport.csproj;
-dotnet pack --no-build -c Release Davenport.Fsharp/Davenport.Fsharp.fsproj
+dotnet pack --no-build -c Release Davenport/davenport.fsproj;
 
-$csnupkg = (gci "Davenport/bin/Release/*.nupkg")[0];
-$fsnupkg = (gci "Davenport.Fsharp/bin/Release/*.nupkg")[0];
+$nupkg = (gci "Davenport/bin/Release/*.nupkg")[0];
 
 # Push the nuget package to AppVeyor's artifact list.
-Push-AppveyorArtifact $csnupkg.FullName -FileName $csnupkg.Name -DeploymentName "davenport.nupkg";
-Push-AppveyorArtifact $fsnupkg.FullName -FileName $fsnupkg.Name -DeploymentName "davenport.fsharp.nupkg"
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "davenport.nupkg";
