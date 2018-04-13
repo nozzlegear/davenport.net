1fb493a0b8c3ae83e3afb407a2059fc1b26aba61
Joshua Harms <joshua@nozzlegear.com>
2018-04-12 21:34:09 -0500

    AppVeyor should use paket to pack nuget files.

--- ps/build_script.ps1
+++ ps/build_script.ps1
@@ -1,8 +1,9 @@
 dotnet restore;
 dotnet build -c Release;
 dotnet pack --no-build -c Release Davenport/Davenport.fsproj;
+./.paket/paket.exe pack --lock-dependencies "nuget"
 
-$nupkg = (gci "Davenport/bin/Release/*.nupkg")[0];
+$nupkg = (gci "nuget/*.nupkg")[0];
 
 # Push the nuget package to AppVeyor's artifact list.
 Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "davenport.nupkg";
